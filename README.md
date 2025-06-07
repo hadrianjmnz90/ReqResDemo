45242189d1855bca0a00442d8a352d9ead68aa76
Adrian Jimenez <hadrianjmnz90@gmail.com>
2025-06-07 11:25:45 -0600

    fixed api key

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ReqResDemo
+
+try 1
--- src/test/java/org/reqres/ReqResBase.java
+++ src/test/java/org/reqres/ReqResBase.java
@@ -1,12 +1,19 @@
 package org.reqres;
 
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
+
+import static io.restassured.RestAssured.given;
 
 public class ReqResBase {
 
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = "https://reqres.in/";
+        RequestSpecification requestSpec = given()
+                .header("x-api-key", "reqres-free-v1");
+
+        RestAssured.requestSpecification = requestSpec;
     }
 }
